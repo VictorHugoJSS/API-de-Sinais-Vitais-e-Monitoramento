d53fe7051a4815b268ff6911118e59f3149cfc1d
VictorHugoJSS <vh9325862@gmail.com>
2025-11-09 15:17:47 -0300

    feita a conexão com o database

--- api/Database.py
+++ api/Database.py
@@ -1,18 +1,61 @@
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import scoped_session, sessionmaker
-import os
+import psycopg2
+CONNECTION=f"postgres://vitalsapp:test@timescaledb/vitals"
 
-postgres_username = os.getenv("POSTGRES_USERNAME")
-postgres_pw = os.getenv("POSTGRES_PW")
-postgres_db = os.getenv("POSTGRES_DATABASE")
-postgres_host = os.getenv("POSTGRES_H")
-
-engine = create_engine(f"postgres://{postgres_username}:{postgres_pw}@{postgres_host}/{postgres_db}")
-metadata = MetaData()
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+conn = psycopg2.connect(CONNECTION)
+cursor = conn.cursor()
 
 def init_db():
-    metadata.create_all(bind=engine)
+    cursor.execute("""DROP TABLE IF EXISTS alerts,vitals,devices""")
+    # cursor.execute("""DROP TABLE IF EXISTS devices""")
+    # cursor.execute("""DROP TABLE IF EXISTS  vitals""")
     
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS alerts(
+                       id serial,
+                       alert_type TEXT NOT NULL,
+                       message TEXT NOT NULL,
+                       timestamp date
+                   )
+                   WITH(
+                       tsdb.hypertable,
+                       tsdb.partition_column='timestamp',
+                       tsdb.segmentby = 'alert_type',
+                       tsdb.orderby = 'timestamp DESC'
+                   )
+                   """)
+    
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS devices(
+                       id serial,
+                       data TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                       Devicename TEXT NOT NULL,
+                       Devicemodel TEXT,
+                       type TEXT,
+                       heart_rate INTEGER,
+                       temperature DOUBLE PRECISION,
+                       blood_pressure DOUBLE PRECISION,
+                       respiratory_rate INTEGER
+                   )
+                   WITH(
+                       tsdb.hypertable,
+                       tsdb.partition_column ='data',
+                       tsdb.segmentby = 'Devicename',
+                       tsdb.orderby = 'data DESC'
+                   )
+                   """)
+    cursor.execute("""
+                   CREATE TABLE IF NOT EXISTS vitals(
+                       id serial,
+                       pacientName TEXT,
+                       cpf TEXT PRIMARY KEY,
+                       temperature DOUBLE PRECISION,
+                       heart_rate INTEGER,
+                       blood_pressure DOUBLE PRECISION,
+                       respiratory_rate INTEGER,
+                       Devicename TEXT
+                   )
+                   """)
+    
+    cursor.execute("""CALL add_columnstore_policy('devices', INTERVAL '1 days', if_not_exists => TRUE);""")
+    cursor.execute("""CALL add_columnstore_policy('alerts', INTERVAL '1 days', if_not_exists => TRUE);""")
+    conn.commit()
